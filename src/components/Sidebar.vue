809048bb9115643c0eef6bb3e71816554c83cdbd
Victoria Rozhkova <victoria.rozhkova@ro.ru>
2022-10-06 17:19:16 +0400

    navigation

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{ open: isOpen }">
+    <li>
+      <router-link
+        v-for="link in links"
+        :key="link.to"
+        :to="link.to"
+        custom
+        v-slot="{ href, navigate, isExactActive }"
+      >
+        <li :class="[isExactActive && 'active']">
+          <a
+            :href="href"
+            @click="navigate"
+            class="waves-effect waves-orange pointer"
+            >{{ link.title }}
+          </a>
+        </li>
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+  props: {
+    isOpen: Boolean,
+  },
+  data: () => ({
+    links: [
+      { title: "Счет", to: "/" },
+      { title: "История", to: "/history" },
+      { title: "Планирование", to: "/planning" },
+      { title: "Новая запись", to: "/record" },
+      { title: "Категории", to: "/categories" },
+    ],
+  }),
+};
+</script>
