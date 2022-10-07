f45d7b3813e6dda3e62cf0eb423bbf18d17c5e30
Victoria Rozhkova <victoria.rozhkova@ro.ru>
2022-10-07 18:31:01 +0400

    validate login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,30 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="onSubmit">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
-        <input id="email" type="text" class="validate" />
+        <input
+          id="email"
+          type="text"
+          class="validate"
+          v-model.trim="email"
+        />
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small class="helper-text invalid" v-if="v$.email.$error">{{
+          v$.email.$errors[0].$message
+        }}</small>
       </div>
       <div class="input-field">
-        <input id="password" type="password" class="validate" />
+        <input
+          id="password"
+          type="password"
+          class="validate"
+          v-model.trim="password"
+        />
         <label for="password">Пароль</label>
-        <small class="helper-text invalid">Password</small>
+        <small class="helper-text invalid" v-if="v$.password.$error">{{
+          v$.password.$errors[0].$message
+        }}</small>
       </div>
     </div>
     <div class="card-action">
@@ -22,15 +36,40 @@
       </div>
       <p class="center">
         Нет аккаунта?
-        <a href="/">Зарегистрироваться</a>
+        <router-link to="/register">Зарегистрироваться</router-link>
       </p>
     </div>
   </form>
 </template>
 
 <script>
+import { useVuelidate } from "@vuelidate/core";
+import { required, email, minLength } from "@vuelidate/validators";
 export default {
   name: "Login",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  setup() {
+    return { v$: useVuelidate() };
+  },
+  validations() {
+    return {
+      email: { email, required },
+      password: { required, minLength: minLength(6) },
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.v$.$validate();
+      if (!this.v$.$error) {
+        this.$router.push("/");
+      }
+    },
+  },
 };
 </script>
 
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,14 +3,23 @@
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
-        <input id="email" type="text" />
+        <input id="email" type="text" v-model.trim="email" />
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small class="helper-text invalid" v-if="v$.email.$error">{{
+          v$.email.$errors[0].$message
+        }}</small>
       </div>
       <div class="input-field">
-        <input id="password" type="password" class="validate" />
+        <input
+          id="password"
+          type="password"
+          class="validate"
+          v-model.trim="password"
+        />
         <label for="password">Пароль</label>
-        <small class="helper-text invalid">Password</small>
+        <small class="helper-text invalid" v-if="v$.password.$error">{{
+          v$.password.$errors[0].$message
+        }}</small>
       </div>
       <div class="input-field">
         <input id="name" type="text" class="validate" />
@@ -34,15 +43,40 @@
 
       <p class="center">
         Уже есть аккаунт?
-        <a href="/">Войти!</a>
+        <router-link to="/login">Войти!</router-link>
       </p>
     </div>
   </form>
 </template>
 
 <script>
+import { useVuelidate } from "@vuelidate/core";
+import { required, email, minLength } from "@vuelidate/validators";
 export default {
   name: "Register",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  setup() {
+    return { v$: useVuelidate() };
+  },
+  validations() {
+    return {
+      email: { email, required },
+      password: { required, minLength: minLength(6) },
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.v$.$validate();
+      if (!this.v$.$error) {
+        this.$router.push("/");
+      }
+    },
+  },
 };
 </script>
 
